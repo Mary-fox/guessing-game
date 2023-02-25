873e4546e6ef4fca90f922a7e5572ac46c7790fd
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-02-26 00:01:14 +0300

    feat: task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        this.guessingValue = Math.round(((this.max - this.min) / 2) + this.min);
+        return this.guessingValue;
     }
 
     lower() {
-
+        this.max = this.guessingValue;
     }
 
     greater() {
-
+        this.min = this.guessingValue;
     }
 }
 

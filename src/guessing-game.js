caa418f54048459569bbb31d35dc964310a1dc09
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-02-26 19:53:33 +0300

    feat:task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -4,10 +4,11 @@ class GuessingGame {
     setRange(min, max) {
         this.max = max;
         this.min = min;
+        this.guessingValue = 0;
     }
 
     guess() {
-        this.guessingValue = Math.round(((this.max - this.min) / 2) + this.min);
+        this.guessingValue = Math.round((this.min + this.max) / 2);
         return this.guessingValue;
     }
 
